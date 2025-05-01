959a2ceca6634174bdd5c0419e02cb4346ed1512
Albert Jacob <killbot2614@protonmail.com>
2025-05-01 15:33:18 +0300

    Add 2.1.1.8.py

---
+++ 2.1.1.8.py
@@ -0,0 +1,23 @@
+# 2.1.1.18 LAB: The print() function 
+
+"""
+Estimated time
+
+5-10 minutes
+Level of difficulty
+
+Very Easy
+Objectives
+- Becoming familiar with the print() function and its formatting capabilities;
+- Experimenting with Python code.
+
+Scenario:
+- Modify the first line of code in the editor, using the sep and end keywords, to match the expected output. Use the two print() functions in the editor.
+- Don't change anything in the second print() invocation.
+
+Expected Output:
+Programming***Essentials***in...Python
+"""
+# Solution
+print("Programming","Essentials","in", sep="***", end="...")
+print("Python") 
