00627aa2c0760c540593e911d378cfd6cda8f1d7
Albert Jacob <killbot2614@protonmail.com>
2025-05-01 12:15:16 +0300

    Update 2.1.1.6.py

--- 2.1.1.6.py
+++ 2.1.1.6.py
@@ -1,37 +1,41 @@
 # 2.1.1.6 LAB: The print() function
 
-'''
-Estimated time
+"""
+Estimated Time: 5-10 minutes
+Level of Difficulty: Very easy
 
-5-10 minutes
-Level of difficulty
+Objectives:
+- Become familiar with the print() function and its formatting capabilities.
+- Experiment with Python code.
 
-Very easy
-Objectives
-- becoming familiar with the print() function and its formatting capabilities;
-- experimenting with Python code.
-
-Scenario
+Scenario:
 The print() command, which is one of the easiest directives in Python, simply prints out a line to the screen.
 
-In your first lab:
-- use the print() function to print the line Hello, Python! to the screen. Use double quotes around the string;
-- having done that, use the print() function again, but this time print your first name;
-- remove the double quotes and run your code. Watch Python's reaction. What kind of error is thrown?
-- then, remove the parentheses, put back the double quotes, and run your code again. What kind of error is thrown this time?
-- experiment as much as you can. Change double quotes to single quotes, use multiple print() functions on the same line, and then on different lines. See what happens.
-'''
+In this lab, you will:
+1. Use the print() function to print the line "Hello, Python!" to the screen.
+2. Print your first name using the print() function.
+3. Remove the double quotes and run your code to observe the error thrown.
+4. Remove the parentheses, put back the double quotes, and run your code again to observe the error thrown.
+5. Experiment with different variations of the print() function.
+"""
 
 # Solution
 
-# 1
+# 1. Print a greeting message
 print("Hello, Python!")
 
-# 2
+# 2. Print your name
 print("Killbot")
 
-# 3
-# NameError
+# 3. Demonstrating NameError by removing quotes
+# Uncomment the line below to see the error
+# print(Killbot)  # This will raise a NameError
 
-# 4
-# SyntaxError
+# 4. Demonstrating SyntaxError by removing parentheses
+# Uncomment the line below to see the error
+# print "Hello, Python!"  # This will raise a SyntaxError
+
+# 5. Experimentation
+print('Single quotes work too!')
+print("Multiple print statements on the same line:", end=' ')
+print("This is possible!")
