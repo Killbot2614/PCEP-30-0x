551ae0cab29490f30cf39710958de458a5e05684
Albert Jacob <killbot2614@protonmail.com>
2025-05-02 12:05:40 +0300

    Add 2.2.1.11.py

---
+++ 2.2.1.11.py
@@ -0,0 +1,32 @@
+#  2.2.1.11 LAB: Python literals - strings
+
+'''
+Estimated time:
+5-10 minutes
+
+Level of difficulty:
+Easy
+
+Objectives:
+- Becoming familiar with the print() function and its formatting capabilities;
+- Practicing coding strings;
+- Experimenting with Python code.
+
+Scenario:
+Write a one-line piece of code, using the print() function, as well as the newline and escape characters, to match the expected result outputted on three lines.
+
+Expected output:
+"I'm"
+""learning""
+"""Python"""
+'''
+
+# Solution
+
+print('"I\'m"')
+print('""learning""')
+print('"""Python"""')
+
+# Another solution
+
+print('"I\'m"\n""learning""\n"""Python"""')
